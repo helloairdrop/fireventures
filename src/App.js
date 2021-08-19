709e21be27d4cf30047ef581df6ce129652f301c
NhonNguyen <vi.nhon.53th@gamil.com>
2021-08-19 22:31:37 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import introVideo from "./assets/videos/intro.mp4";
 import telegramImg from "./assets/images/telegram.png";
+import telegramImg2 from "./assets/images/tele2.png";
+import telegramImg3 from "./assets/images/tele3.png";
+import ytb from "./assets/images/ytb.png";
+import tw from "./assets/images/twitter.png";
 
 function App() {
   return (
     <div className="wrapper">
-      <video autoPlay muted loop id="myVideo">
+      <video autoPlay muted loop id="myVideo" playsInline>
         <source src={introVideo} type="video/mp4" />
       </video>
 
@@ -19,14 +23,61 @@ function App() {
       </div>
 
       <footer>
-        <a
-          href="https://t.me/FIREVenture"
-          target="_blank"
-          rel="noreferrer"
-          className="telegram-link"
-        >
-          <img src={telegramImg} alt="" />
-        </a>
+        <div>
+          <a
+            href="https://t.me/FIREVenture"
+            target="_blank"
+            rel="noreferrer"
+            className="telegram-link"
+            title="Chat"
+          >
+            <img src={telegramImg} alt="" />
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://t.me/Fire_Ventures"
+            target="_blank"
+            rel="noreferrer"
+            className="telegram-link"
+            title="Kênh NEWS"
+          >
+            <img src={telegramImg2} alt="" />
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://t.me/FIREVentures_GEM"
+            target="_blank"
+            rel="noreferrer"
+            className="telegram-link"
+            title="Kênh soi kèo Gem Hunter"
+          >
+            <img src={telegramImg3} alt="" />
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://www.youtube.com/watch?v=IrH-H724NOY"
+            target="_blank"
+            rel="noreferrer"
+            className="telegram-link"
+            title="Youtube"
+          >
+            <img src={ytb} alt="" />
+          </a>
+        </div>
+        <div>
+          <a
+            href="https://twitter.com/FIREVENTURESnet"
+            target="_blank"
+            rel="noreferrer"
+            className="telegram-link"
+            title="Youtube"
+          >
+            <img src={tw} alt="" />
+          </a>
+        </div>
       </footer>
     </div>
   );
